d847cc43ed799386406bb93be9ba567b183d0767
JayEffTree <jfinelli3@gmail.com>
2016-03-29 14:30:38 -0600

    Version 1.2
    Added new commands for owners.
    
    -Kick
    -Ban
    -Mute
    
    Fixed issue with paying respects to be only 'f' or 'F' now.

--- respectsbot.js
+++ respectsbot.js
@@ -40,16 +40,16 @@ status.ignore={
 }
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-  console.log(e.message.author.username + " | " + e.message.content)
+  //console.log(e.message.author.username + " | " + e.message.content)
   //invite system
   if(e.message.author.id == config.botid && e.message.isPrivate) return
     if(e.message.isPrivate && e.message.content.indexOf("discord.gg") >= 0){
       var code = /https?:\/\/discord\.gg\/([A-Za-z0-9-]+)\/?/.exec(e.message.content)[1];
       client.Invites.resolve(code).then(function(value) {
-              if(client.Guilds.find(g => g.name == value.guild.name) >= 1){
-                e.message.channel.sendMessage("already there")
-                return
-              }else{
+				if(client.Guilds.find(g => g.name == value.guild.name) >= 1){
+					e.message.channel.sendMessage("already there")
+						return
+				}else{
           client.Invites.accept(code)
           e.message.channel.sendMessage(":ok_hand:")
         }
@@ -75,16 +75,21 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 	};
     if(exists("channels",e.message.channel.id)==false){
       //commands start here
-      if (msg[0] == "f" || msg[0] == "F"){
+      if (e.message.content == "f" || e.message.content == "F"){
         respectsPaid++
         e.message.channel.sendMessage(e.message.author.username + " has paid their respects. :eyes:\nRespects paid so far: **" + respectsPaid + "**");
         return
       }
       if(msg[0] == config.prefix + "help" || msg[0] == config.prefix + "cmd"){
-        e.message.channel.sendMessage("`" + config.prefix + "block | " + config.prefix + "help | " + config.prefix + "cmd | " + config.prefix + "eval | 'f' or 'F' to pay respects.`")
+        e.message.channel.sendMessage("`(OP)" + config.prefix + "block | " + config.prefix + "help | " + config.prefix + "cmd | (OP)" + config.prefix + "eval | (OP)" + config.prefix + "tools | " + config.prefix + "i | 'f' or 'F' to pay respects.`")
         return
       }
-      if(msg[0] == config.prefix + "block"){
+			if(msg[0] == config.prefix + "i"){
+				var mentionedUser = e.message.mentions[0];
+				if(!mentionedUser) return e.message.channel.sendMessage(":thumbsdown: no mention")
+				e.message.channel.sendMessage("```javascript\n" + require("util").inspect(client.Users.get(mentionedUser.id).memberOf(e.message.channel.guild)) + "```")
+			}
+      if(msg[0] == config.prefix + "block" && e.message.author.id === config.bot_owner){
         if(msg[1] == "channel"){
           status.ignore["channels"].push(e.message.channel.id);
           require('fs').writeFileSync(config.blocklist, JSON.stringify(status.ignore, null, '\t'))
@@ -92,6 +97,39 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
           return
         }else return e.message.channel.sendMessage(":thumbsdown:")
       }
+			if(msg[0] == config.prefix + "tools" && e.message.author.id == config.bot_owner){
+				if(msg[1] == "mute"){
+					var mentionedUser = e.message.mentions[0];
+					if(!mentionedUser) return e.message.channel.sendMessage("no user found")
+					if(client.Users.get(mentionedUser.id).memberOf(e.message.channel.guild).mute){
+						client.Users.get(mentionedUser.id).memberOf(e.message.channel.guild).serverUnmute()
+						e.message.channel.sendMessage("unmuted :thumbsdown:")
+						return
+					}else{
+						client.Users.get(mentionedUser.id).memberOf(e.message.channel.guild).serverMute()
+						e.message.channel.sendMessage("muted :ok_hand:")
+						return
+					}
+				}else if(msg[1] == "kick"){
+						var mentionedUser = e.message.mentions[0];
+						if(!mentionedUser) return e.message.channel.sendMessage("no user found")
+						client.Users.get(mentionedUser.id).memberOf(e.message.channel.guild).kick().then(function(success) {
+							e.message.channel.sendMessage("kicked " + mentionedUser.username.toLowerCase())
+			      }, function() {
+			        e.message.channel.sendMessage(":thumbsdown: no perms")
+							return
+			      });
+				}else if(msg[1] == "ban"){
+						var mentionedUser = e.message.mentions[0];
+						if(!mentionedUser) return e.message.channel.sendMessage("no user found")
+						client.Users.get(mentionedUser.id).memberOf(e.message.channel.guild).ban(7).then(function(success) {
+							e.message.channel.sendMessage("banned " + mentionedUser.username.toLowerCase())
+			      }, function() {
+			        e.message.channel.sendMessage(":thumbsdown: no perms")
+							return
+			      });
+				}
+			}
       if(msg[0] == config.prefix + "eval" && e.message.author.id === config.bot_owner){
         try{
 					e.message.channel.sendMessage("```javascript\n" + eval(args) + "```")
